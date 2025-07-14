b8677714d9b4b874242f0a51facf9d9b425a6459
farid-ss3945 <abbaslifarid2006@gmail.com>
2025-07-14 11:41:44 +0400

    Add files via upload

---
+++ dz1.sql
@@ -0,0 +1,30 @@
+create database Academy;
+
+use Academy;
+
+create table Groups(
+    [Id] int not null primary key identity(1, 1),
+    [Name] nvarchar(10) not null CHECK (name <> '') unique,
+    [Rating] int not null check([Year]>=0 and [Year]<=5),
+    [Year] int not null check([Year]>=1 and [Year]<5)
+);
+
+create table Departments(
+    [Id] int not null primary key identity(1, 1),
+    [Name] nvarchar(100) not null CHECK (name <> '') unique,
+    [Rating] int not null check([Rating]>=0) default(0)
+)
+
+create table Faculties(
+    [Id] int not null primary key identity(1, 1),
+    [Name] nvarchar(100) not null CHECK (name <> '') unique
+)
+
+create table Teachers(
+    [Id] int not null primary key identity(1, 1),
+    [EmploymentDate] date not null CHECK(Year([EmploymentDate])>=1990),
+    [Name] nvarchar(max) not null CHECK (name <> ''),
+    [Premium] money not null CHECK([Premium]>=0) default(0),
+    [Salary] money not null CHECK([Salary]>0),
+    [Surname] nvarchar(max) not null CHECK (name <> '')
+)
