a1726bbbe2f65d93f64f3f0f5ccc7deeb87c746e
farid-ss3945 <abbaslifarid2006@gmail.com>
2025-07-16 11:33:05 +0400

    Update dz1.sql

--- dz1.sql
+++ dz1.sql
@@ -11,21 +11,21 @@ create table Groups(
 
 create table Departments(
     [Id] int not null primary key identity(1, 1),
-    [Name] nvarchar(100) not null CHECK (name <> '') unique,
+    [Name] nvarchar(100) not null CHECK (Name <> '') unique,
     [Rating] int not null CHECK([Rating]>=0) default(0),
     [Financing] money not null CHECK([Financing]>=0) default(0)
 )
 
 create table Faculties(
     [Id] int not null primary key identity(1, 1),
-    [Name] nvarchar(100) not null CHECK (name <> '') unique
+    [Name] nvarchar(100) not null CHECK (Name <> '') unique
 )
 
 create table Teachers(
     [Id] int not null primary key identity(1, 1),
     [EmploymentDate] date not null CHECK(Year([EmploymentDate])>=1990),
     [Name] nvarchar(max) not null CHECK (Name <> ''),
-    [Premium] money not null CHECK([Premium]>=0) default(0),
+    [Premium] money not null CHECK([Premium]>0) default(0),
     [Salary] money not null CHECK([Salary]>0),
     [Surname] nvarchar(max) not null CHECK (Surname <> '')
 )
