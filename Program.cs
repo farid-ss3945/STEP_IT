e2a036d75f75b5ac4f3c4107b108d776afb8864e
farid-ss3945 <abbaslifarid2006@gmail.com>
2025-07-23 09:27:22 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
 
 var configBuilder = new ConfigurationBuilder();
 configBuilder.AddJsonFile("Dz1.json");
